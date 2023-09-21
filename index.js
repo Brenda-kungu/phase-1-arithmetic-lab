c69a2b803f121915e7139e1ab8431bc08ef1a95f
Brenda <brendakungu3@gmail.com>
2023-09-22 00:35:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+
+const random = 
+Math.ceil(Math.random()*10) + 1;
+Math.ceil(Math.random() *10) +1;
+console.log(Math.random())
+console.log(Math.ceil(Math.random()))
+console.log(Math.ceil(Math.random()))
+
+const num3 = 16;
+const num4 = 4;
+const mod = num3/num4
+let counter = 4;
+
+
+
+Math.max(3,8,4,20,1,9)
+const max = 20
+console.log(Math.max(3,8,20,7))
+
+
+
+
+
+
+
+
+
+
+
+
